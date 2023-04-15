df844902b25116543e233e9fe8cb4c71bf440f0d
Dan <dangerchannle@gmail.com>
2023-04-15 18:34:41 +0300

    nav usability, observe and fix some issues

--- js/script.js
+++ js/script.js
@@ -2,3 +2,23 @@ window.addEventListener('scroll', function(){
     let header = document.querySelector('header')
     header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if (entry.isIntersecting){
+                document.querySelectorAll('.nav-item').forEach((link) =>{
+                    link.getAttribute('href').replace('#', '');
+                    if(link.getAttribute('href').replace('#', '') === entry.target.id){
+                        link.classList.add('nav-item--active')
+                    }else{
+                        link.classList.remove('nav-item--active')
+                    }
+                })
+        }
+    })
+}, {
+    threshold: 0.5
+});
+
+document.querySelectorAll('section').forEach(
+    (section) => observer.observe(section))
